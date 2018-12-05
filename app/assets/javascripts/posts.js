f7ecec83e765464c9f4e327e2263c9d15c9c3cf9
Yony <arleybri18@gmail.com>
2018-12-05 00:47:55 -0500

    App hecha en clase practica AJAX JSON RAILS

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,25 +1,37 @@
-  function PostNewView(){
+  function PostNewView() {
     alert("Hola Jade")
   }
 
-  function PostIndexView(){
-    $('form').submit(function(e){
+  function PostIndexView() {
+    $('form').submit(function (e) {
       e.preventDefault()
       var $form = $(this);
-  var url = $form.attr('action');
+      var url = $form.attr('action');
 
-  $.ajax({
-         type: "POST",
-         url: url,
-         data: $form.serialize(), // serializes the form's elements.
-         contentType: 'application/json',   
-         success: function(data)
-         {
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $form.serialize(), // serializes the form's elements.
+        dataType: 'json',
+        success: function (data) {
 
-            $('ul').prepend("hola Jade")
-              // show response from the php script.
-         }
-       });
+          $('tbody').append(`<tr><td>${data.id}</td><td>${data.title}</td></tr>`)
+          $(':input[type=submit]').prop('disabled', false)
+        }
+      });
+      $form.trigger('reset')
+    })
+    $('boton-post').on('click',function(e){
+      e.preventDefault()
+            $.ajax({
+              type: "GET",
+              url: '/',
+              dataType: 'json',
+              success: function (data) {
+
+               console.log(data)
+              }
+            });
     })
   }
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @post = Post.new
+
+      respond_to do |format|
+        format.html { }
+        format.json {render json:@posts}
+        format.js {}
+      end
+
   end
+
+  def show
+    @post = Post.find(params[:id]) 
+  end
+
 
   def new
     @post = Post.new
@@ -12,7 +24,11 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     if post.save
-      redirect_to '/'
+      # redirect_to '/'
+      respond_to do |format|
+        format.html { redirect_to '/'}
+        format.json {render json:post}
+      end
     end
   end
 
